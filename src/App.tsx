b947b6fcecb9426dfdebfafa300ff03aaa9b37db
liuyongjin <572551431@qq.com>
2024-09-03 22:15:27 +0800

    feat: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,13 @@ import { Button } from "antd";
 function App() {
   return (
     <>
-      <div>
-        <Button>123</Button>
+      <div className="flex md:bg-black min-w-10 content-between">
+        <div>
+          <Button>123</Button>
+        </div>
+        <div>
+          <Button>123</Button>
+        </div>
       </div>
     </>
   );
