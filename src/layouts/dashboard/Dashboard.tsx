892d0cc487f82c1ed3c47a9f4fc7dfb2988ac863
liuyongjin <572551431@qq.com>
2024-09-18 16:57:58 +0800

    feat: add nav

--- src/layouts/dashboard/Dashboard.tsx
+++ src/layouts/dashboard/Dashboard.tsx
@@ -1,15 +1,54 @@
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import { Button, Layout, theme } from "antd";
 import { Suspense } from "react";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 import { CircleLoading } from "@/components";
 
+const { Header, Sider, Content } = Layout;
+
 import { Nav } from "./partial/Nav";
 
 export const Dashboard = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
   return (
     <Suspense fallback={<CircleLoading />}>
-      <Nav />
-      <Outlet />
+      <Layout className="h-screen">
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="pt-12" />
+          <Nav />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }}>
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                fontSize: "16px",
+                width: 64,
+                height: 64,
+              }}
+            />
+          </Header>
+          <Content
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <Outlet />
+          </Content>
+        </Layout>
+      </Layout>
     </Suspense>
   );
 };
--- src/layouts/dashboard/partial/Nav.tsx
+++ src/layouts/dashboard/partial/Nav.tsx
@@ -1,3 +1,46 @@
+import { AppstoreOutlined, MailOutlined } from "@ant-design/icons";
+import { Menu, MenuProps } from "antd";
+import { useNavigate } from "react-router-dom";
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+const items: MenuItem[] = [
+  {
+    key: "Dashboard",
+    label: "Dashboard",
+    icon: <MailOutlined />,
+    children: [{ key: "/dashboard/workbench", label: "Workbench" }],
+  },
+  {
+    key: "Management",
+    label: "Management",
+    icon: <AppstoreOutlined />,
+    children: [{ key: "/management/permission", label: "Permission" }],
+  },
+];
+
 export const Nav = () => {
-  return <div>nav</div>;
+  const navigate = useNavigate();
+
+  const handleClick: MenuProps["onClick"] = ({ key }) => {
+    navigate(key);
+  };
+
+  return (
+    <Menu
+      // className="h-full !border-none"
+      theme="dark"
+      mode="inline"
+      // defaultSelectedKeys={["1"]}
+      items={items}
+      // defaultOpenKeys={openKeys}
+      // defaultSelectedKeys={[pathname]}
+      // selectedKeys={[pathname]}
+      // openKeys={openKeys}
+      // onOpenChange={onOpenChange}
+      onClick={handleClick}
+      // style={menuStyle}
+      // inlineCollapsed={collapsed}
+    />
+  );
 };
